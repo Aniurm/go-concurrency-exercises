6492679a418df9e9ceb4b355f39137d9e4640e4c
Aniurm <534147085@qq.com>
2023-12-10 23:24:14 +0800

    feat(3): atomic, tick, channel

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,13 @@
 
 package main
 
+import (
+	"sync/atomic"
+	"time"
+)
+
+const MaxTime = int64(10)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -18,11 +25,38 @@ type User struct {
 	TimeUsed  int64 // in seconds
 }
 
+func (u *User) AddTimeUsed(time int64) int64 {
+	return atomic.AddInt64(&u.TimeUsed, time)
+}
+
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	if u.IsPremium {
+		process()
+		return true
+	}
+	if atomic.LoadInt64(&u.TimeUsed) >= MaxTime {
+		return false
+	}
+
+	done := make(chan bool)
+	go func() {
+		process()
+		done <- true
+	}()
+
+	tick := time.Tick(time.Second)
+	for {
+		select {
+		case <-done:
+			return true
+		case <-tick:
+			if i := u.AddTimeUsed(1); i >= MaxTime {
+				return false
+			}
+		}
+	}
 }
 
 func main() {
